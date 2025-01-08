024ac9b3ac68448758ee243e43a82ef0298eadbb
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-01-08 13:54:36 -0300

    refactor: :recycle: refactor controller

--- server/src/main/java/com/denisson/server/domain/exceptions/EntityNotValidException.java
+++ server/src/main/java/com/denisson/server/domain/exceptions/EntityNotValidException.java
@@ -3,6 +3,6 @@ package com.denisson.server.domain.exceptions;
 public class EntityNotValidException extends RuntimeException{
 
     public EntityNotValidException(Object entity) {
-        super(String.format("Entity %s is not valid!", entity.toString()));
+        super(String.format("Entity %s is not valid!", entity.getClass().getSimpleName()));
     }
 }
--- server/src/main/java/com/denisson/server/interfaceAdapters/controllers/CategoryController.java
+++ server/src/main/java/com/denisson/server/interfaceAdapters/controllers/CategoryController.java
@@ -31,7 +31,7 @@ public class CategoryController {
 
             return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
     
