ff250927d4579374cdf775a38490cbcbbe8feed6
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-01-11 11:31:33 -0300

    feat: :iphone: responsivity header

--- web/src/presentation/pages/public/home/components/header/index.tsx
+++ web/src/presentation/pages/public/home/components/header/index.tsx
@@ -1,17 +1,28 @@
-import './header.styles.sass'
+import { useSpring, animated } from '@react-spring/web';
+import './header.styles.sass';
 
 export const Header = () => {
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    delay: 200,
+    config: { duration: 1000 }, 
+  });
+
   return (
     <div className="header_container">
-        <div className="header_discover">
-            <p className='header_title1'>New Arrival</p>
-            <p className='header_title2'>Discover Our</p>
-            <p className='header_title2'>New Collection</p>
-            <p className='header_txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-            <div className="header_btn">
-                <button>BUY NOW</button>
-            </div>
+      <animated.div className="header_discover" style={props}>
+        <p className="header_title1">New Arrival</p>
+        <p className="header_title2">Discover Our</p>
+        <p className="header_title2">New Collection</p>
+        <p className="header_txt">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+          luctus nec ullamcorper mattis.
+        </p>
+        <div className="header_btn">
+          <button>BUY NOW</button>
         </div>
+      </animated.div>
     </div>
-  )
-}
+  );
+};
