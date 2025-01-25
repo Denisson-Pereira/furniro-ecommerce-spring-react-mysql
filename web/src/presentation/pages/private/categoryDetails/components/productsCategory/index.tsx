f0e091b5e8b08710ca6c021a9aaa52e41f548429
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-01-24 21:06:47 -0300

    feat: :sparkles: update categories products

--- web/src/presentation/pages/private/categoryDetails/components/productsCategory/index.tsx
+++ web/src/presentation/pages/private/categoryDetails/components/productsCategory/index.tsx
@@ -5,10 +5,17 @@ import { useAuthContext } from '../../../../../context/authContext';
 import { getAllProductsServiceLocator } from '../../../../../../infra/services/getProductsServiceLocator';
 import { IProduct } from '../../../../../../core/models/IProduct';
 import { useFavoritiesContext } from '../../../../../context/favoritiesContext';
+import { FaHeart } from 'react-icons/fa';
+import { monetaryUnit } from '../../../../../../utils/monetaryUnit';
+import { promotionValue } from '../../../../../../utils/promotionValue';
+import { useHandlePage } from '../../../../../hooks/useHandlePage';
+import { CiHeart } from 'react-icons/ci';
 
 export const ProductsCategories = () => {
     const { category } = useParams();
+    const handlePage = useHandlePage();
     const { loading, setLoading } = useAuthContext();
+    const { addFavorite, isFavorite} = useFavoritiesContext();
 
     const [products, setProducts] = useState<IProduct[]>([]);
     const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([]);
@@ -36,13 +43,50 @@ export const ProductsCategories = () => {
     }, [category, products]);
 
     return (
-        <div className="categories-container">
+        <div className="productsCategories_container">
             {loading ? (
-                <div className="spinner">Loading...</div>
+                <div className="productsCategories_spinner"></div>
             ) : filteredProducts.length > 0 ? (
-                <div className="product-grid">
+                <div className="productsCategories_box">
                     {filteredProducts.map(product => (
-                        <div></div>
+                        <div
+                            className="productsCategories_card"
+                            key={product.id}
+                        >
+                            <img
+                                src={product.image}
+                                alt="img"
+                                onClick={() => handlePage(product.id)}
+                            />
+                            <div className="productsCategories_favorities_box">
+                                {isFavorite(product) ? (
+                                    <div
+                                    className="productsCategories_icon"
+                                    onClick={() => addFavorite(product)}
+                                >
+                                    <FaHeart />
+                                </div>
+                                ) : (
+                                    <div 
+                                        className="productsCategor_favorities_icon"
+                                        onClick={() => addFavorite(product)}
+                                    >
+                                        <CiHeart />
+                                    </div>
+                                )}
+                            </div>
+                            <div
+                                className="productsCategories_info"
+                                onClick={() => addFavorite(product)}
+                            >
+                                <p>{product.name}</p>
+                                <span>Styllish cafe chair</span>
+                                <div className="productsCategories_info_price">
+                                    <p>{monetaryUnit(product.price)}</p>
+                                    <span>{monetaryUnit(promotionValue(20, product.price))}</span>
+                                </div>
+                            </div>
+                        </div>
                     ))}
                 </div>
             ) : (
