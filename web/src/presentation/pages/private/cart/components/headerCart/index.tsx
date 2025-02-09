2da49ee6079918320509af2352335a54a40f9999
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-02-08 21:50:54 -0300

    feat: :sparkles: update cart

--- web/src/presentation/pages/private/cart/components/headerCart/index.tsx
+++ web/src/presentation/pages/private/cart/components/headerCart/index.tsx
@@ -2,16 +2,19 @@ import { Link } from 'react-router-dom'
 import Logo from '../../../../../../assets/images/logo.png'
 
 import './headerCart.styles.sass'
+import { useTranslation } from 'react-i18next'
 
 export const HeaderCart = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="headerCart_container">
             <img src={Logo} alt="logo" />
-            <p>Cart</p>
+            <p>{t("navbar.cart")}</p>
             <div className="cart_links">
-                <Link className='cart_a' to={'/'}>Home</Link>
+                <Link className='cart_a' to={'/'}>{t("navbar.home")}</Link>
                 <p>&gt;</p>
-                <p>Cart</p>
+                <p>{t("navbar.cart")}</p>
             </div>
         </div>
     )
