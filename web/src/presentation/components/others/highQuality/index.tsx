34c969ad1bebddb6c01eb5f74dbdf8f21b128d99
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2025-02-08 21:00:55 -0300

    feat: :sparkles: update translate

--- web/src/presentation/components/others/highQuality/index.tsx
+++ web/src/presentation/components/others/highQuality/index.tsx
@@ -4,55 +4,57 @@ import Free from '../../../../assets/icons/free.png'
 import Image from '../../../../assets/icons/24.png'
 
 import './highQuality.styles.sass'
+import { useTranslation } from 'react-i18next'
 
 export const HighQuality = () => {
-  return (
-    <div className="highQuality_container">
-        <div className="highQuality_box">
-            <img 
-                src={High} 
-                alt="high" 
-                title='Quality'
-            />
-            <div className="highQuality_box_txt">
-                <p>High Quality</p>
-                <span>Crafted from top materials</span>
-            </div>
-        </div>
-        <div className="highQuality_box">
-            <img 
-                src={Warranty} 
-                alt="warranty" 
-                title='Warranty'
-            />
-            <div className="highQuality_box_txt">
-                <p>Warranty Propection</p>
-                <span>Over 2 years</span>
-            </div>
-        </div>
-        <div className="highQuality_box">
-            <img 
-                src={Free} 
-                alt="free" 
-                title='Free'
-            />
-            <div className="highQuality_box_txt">
-                <p>Free Shipping</p>
-                <span>Order over $150</span>
-            </div>
-        </div>
-        <div className="highQuality_box">
-            <img 
-                src={Image} 
-                alt="24/7" 
-                title='24/7'
-            />
-            <div className="highQuality_box_txt">
-                <p>24/7 Support</p>
-                <span>Dedicated support</span>
-            </div>
-        </div>
+    const { t } = useTranslation();
 
-    </div>
-  )
+    return (
+        <div className="highQuality_container">
+            <div className="highQuality_box">
+                <img 
+                    src={High} 
+                    alt="high" 
+                    title={t('highQuality.high')}
+                />
+                <div className="highQuality_box_txt">
+                    <p>{t('highQuality.high')}</p>
+                    <span>{t('highQuality.crafted')}</span>
+                </div>
+            </div>
+            <div className="highQuality_box">
+                <img 
+                    src={Warranty} 
+                    alt="warranty" 
+                    title={t('highQuality.warranty')}
+                />
+                <div className="highQuality_box_txt">
+                    <p>{t('highQuality.warranty')}</p>
+                    <span>{t('highQuality.over')}</span>
+                </div>
+            </div>
+            <div className="highQuality_box">
+                <img 
+                    src={Free} 
+                    alt="free" 
+                    title={t('highQuality.free')}
+                />
+                <div className="highQuality_box_txt">
+                    <p>{t('highQuality.free')}</p>
+                    <span>{t('highQuality.order')}</span>
+                </div>
+            </div>
+            <div className="highQuality_box">
+                <img 
+                    src={Image} 
+                    alt="24/7" 
+                    title={t('highQuality.24')}
+                />
+                <div className="highQuality_box_txt">
+                    <p>{t('highQuality.24')}</p>
+                    <span>{t('highQuality.delicated')}</span>
+                </div>
+            </div>
+        </div>
+    )
 }
